a3d6e33b6628a8b4d350af1246521796f75171d6
ajithmanmadhan <ajithmanmu@gmail.com>
2020-11-25 16:16:01 -0800

    Update execute.sh

--- scraper-action/execute.sh
+++ scraper-action/execute.sh
@@ -1,6 +1,8 @@
-#!/bin/sh -l
+#!/bin/bash
 
 array=( aston-martin audi bentley bmw citroen datsun ferrari force ford great-wall haima honda hyundai isuzu jaguar jeep kia lamborghini land-rover lexus mahindra maruti-suzuki maserati mercedes-benz mg mini mitsubishi nissan porsche renault rolls-royce skoda tata toyota volkswagen volvo )
+# declare -a array=("element1" "element2" "element3")
+
 now=$(date)
 echo "Script started for date: $now"
 for i in "${array[@]}"
@@ -9,7 +11,7 @@ do
     # curl -i --write-out "%{http_code}\n" --silent --output /dev/null  --header 'Host: localhost:3000' "http://localhost:3000/api/cron?brand=${i}"
     # curl -i --write-out "%{http_code}\n" --silent --output /dev/null  --header 'Host: auto-scraper.vercel.app' "https://auto-scraper.vercel.app/api/cron?brand=${i}"
     echo "Completed $i"
-    sleep 10
+    # sleep 10
 done
 echo "Script completed for date: $now"
 exit
